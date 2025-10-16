4f897108cce9f5c7b0b0985291e306ca0afd9921
Shahab <garivanishahabedin@gmail.com>
2025-10-16 16:32:29 +0330

    fix: fix access modifiers in db.py

--- src/TODOListProject/db.py
+++ src/TODOListProject/db.py
@@ -24,41 +24,41 @@ class DBInterface(ABC):
 
 class InMemoryDB(DBInterface):
     def __init__(self):
-        self.projects = {}
-        self.tasks = {}
+        self.__projects = {}
+        self.__tasks = {}
 
     def get_all(self, table: str) -> list[Any] | None:
         match table:
             case "projects":
-                return list(self.projects.values())
+                return list(self.__projects.values())
             case "tasks":
-                return list(self.tasks.values())
+                return list(self.__tasks.values())
             case _:
                 raise ValueError("Invalid table")
 
     def get_by_id(self, table: str, entity_id: int) -> object | None:
         match table:
             case "projects":
-                return self.projects[entity_id]
+                return self.__projects[entity_id]
             case "tasks":
-                return self.tasks[entity_id]
+                return self.__tasks[entity_id]
             case _:
                 raise ValueError("Invalid table")
 
     def update_or_insert(self, table: str, entity: NamedEntity) -> None:
         match table:
             case "projects":
-                self.projects[entity.entity_id] = entity
+                self.__projects[entity.entity_id] = entity
             case "tasks":
-                self.tasks[entity.entity_id] = entity
+                self.__tasks[entity.entity_id] = entity
             case _:
                 raise ValueError("Invalid table")
 
     def delete(self, table: str, entity_id: int) -> None:
         match table:
             case "projects":
-                del self.projects[entity_id]
+                del self.__projects[entity_id]
             case "tasks":
-                del self.tasks[entity_id]
+                del self.__tasks[entity_id]
             case _:
                 raise ValueError("Invalid table")
