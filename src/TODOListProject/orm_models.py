06c01439578d373428a80671187fc55bc152d93b
Shahab <garivanishahabedin@gmail.com>
2025-11-29 11:17:37 +0330

    fix: add unique constraint to the name column in task and project classes

--- src/TODOListProject/orm_models.py
+++ src/TODOListProject/orm_models.py
@@ -15,7 +15,7 @@ class Base(DeclarativeBase):
 class Project(Base):
     __tablename__ = "projects"
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str]
+    name: Mapped[str] = mapped_column(unique=True)
     description: Mapped[Optional[str]]
     tasks: Mapped[List["Task"]] = relationship(back_populates="project")
 
@@ -23,7 +23,7 @@ class Project(Base):
 class Task(Base):
     __tablename__ = "tasks"
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str]
+    name: Mapped[str] = mapped_column(unique=True)
     description: Mapped[Optional[str]]
     status: Mapped[TaskStatus] = mapped_column(Enum(TaskStatus, name="task_status"), default=TaskStatus.todo)
     deadline: Mapped[Optional[datetime]]
