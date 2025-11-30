90f3e8ab3fdcc2d67eb6b505563de0b3e2ded393
Shahab <garivanishahabedin@gmail.com>
2025-11-30 20:37:03 +0330

    feat: implement relational database interface to replace in-memory database

--- src/TODOListProject/main.py
+++ src/TODOListProject/main.py
@@ -1,6 +1,7 @@
 import src.TODOListProject.cli as cli
 from src.TODOListProject.controller import ProjectTaskController
-from src.TODOListProject.db import InMemoryDB
+from src.TODOListProject.db import InMemoryDB, RelationalDB
 
 if __name__ == "__main__":
-    cli.run_cli(ProjectTaskController(InMemoryDB()))
+    # cli.run_cli(ProjectTaskController(InMemoryDB()))
+    cli.run_cli(ProjectTaskController(RelationalDB('postgresql://postgres:1234@localhost/todolist')))
